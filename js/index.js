7e81349b49661b5b3e1a0133260b7d91f5eaef66
DMovsumov <movsumov.denis@gmail.com>
2019-11-24 19:22:23 +0500

    создан выбор валюты

--- js/index.js
+++ js/index.js
@@ -8,12 +8,37 @@ function load() {
         let table = document.querySelector('#currency')
         let count = 1
         let html = data.map((item)=>{
-            return '<tr>'+ '<th>'+ count++ + '</th>' + '<th>'+ item.name + '</th>' + '<th>'+ item.price_usd +'$' + '</th>' + '<th>'+ item.price_btc + '</th>' + '<th>'+ item.total_supply + '</th>' + '<th>'+ item.max_supply + '</th>' + '<th>'+ item.percent_change_1h + '</th>' + '<th>'+ item.percent_change_24h + '</th>' + '<th>'+ item.percent_change_7d + '</th>' + '</tr>'
+            return '<tr class="info">' + '<th>'+ count++ + '</th>' + '<th>'+ item.name + '</th>' + '<th class="price_usdConv">' + converter(item.price_usd) + '</th>' + '<th>'+ item.price_btc + '</th>' + '<th>'+ item.total_supply + '</th>' + '<th>'+ item.max_supply + '</th>' + '<th>'+ item.percent_change_1h + '</th>' + '<th>'+ item.percent_change_24h + '</th>' + '<th>'+ item.percent_change_7d + '</th>' + '</tr>'
         })
         
+        table.insertAdjacentHTML('beforeEnd', html.join(' '))
 
-        table.insertAdjacentHTML('afterbegin', html.join(' '))
-        console.log(data)        
+
     })
 }
 load()
+
+const options = document.querySelector('.pirce_usd')
+const currency = ['USD' ,'RUB', 'EURO', 'GBP', 'JPY', 'CHF']
+let item = document.createElement('option')
+let price_usd = document.querySelectorAll('.price_usdConv')
+
+function currency_select(price) {    
+    for(let i =0; i < currency.length; i++){
+        item.text = currency[i]
+        item.value = currency[i]
+        options.appendChild(item.cloneNode(true))        
+    }     
+}
+
+currency_select()
+
+function converter(price) {
+    options.addEventListener('change', () => {
+        if(options.value == 'RUB'){
+            return price * 63
+        } else {
+            return price
+        }
+    })      
+}
